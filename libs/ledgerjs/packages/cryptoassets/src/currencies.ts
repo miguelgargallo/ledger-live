293f47f5d799af1f7f7f019e6f9a25cc3328f536
lvndry <lvndry@proton.me>
2022-11-25 12:55:04 +0100

    use for...of loop to register currencies

--- libs/ledgerjs/packages/cryptoassets/src/currencies.ts
+++ libs/ledgerjs/packages/cryptoassets/src/currencies.ts
@@ -3467,9 +3467,9 @@ const prodCryptoArray: CryptoCurrency[] = [];
 const cryptocurrenciesArrayWithoutTerminated: CryptoCurrency[] = [];
 const prodCryptoArrayWithoutTerminated: CryptoCurrency[] = [];
 
-Object.values(cryptocurrenciesById).forEach((cryptoCurrency) => {
+for (const cryptoCurrency of Object.values(cryptocurrenciesById)) {
   registerCryptoCurrency(cryptoCurrency);
-});
+}
 
 /**
  *
